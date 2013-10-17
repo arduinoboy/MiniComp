757644711d6eee232971cb53ddd704880783a850
ArduinoBoy <BJCandari@CDesign-MacBook-Pro.local>
2013-10-16 18:35:58 -1000

    Added scetch

---
+++ MiniComp/MiniComp.ino
@@ -0,0 +1,63 @@
+/*
+MiniComp baseline firmware
+*/
+
+/*
+MiniComp G-M-B
+*/
+
+/*
+Preliminary Firmware for MiniComp Badge
+
+Functions
+
+Boot Screen
+
+Three Badge-amations
+
+Bade on Boot
+
+*/
+
+// Includes
+
+#include "PCD8544.h"
+
+
+// Defs
+
+
+// pin 7 - Serial clock out (SCLK)
+// pin 6 - Serial data out (DIN)
+// pin 5 - Data/Command select (D/C)
+// pin 4 - LCD chip select (CS)
+// pin 3 - LCD reset (RST)
+PCD8544 nokia = PCD8544(7, 6, 5, 4, 3);
+
+
+
+//Setup
+
+
+void setup(void) {  
+  intlcdm();
+}
+
+
+void loop() {
+  
+}
+
+
+//Functions
+
+//Boot Screen
+void intlcdm(){
+  nokia.println("MiniComp");
+  nokia.println("V0.1B");
+  nokia.println("Josephs Badge");
+  nokia.display();        // show the changes to the buffe
+  delay(1000);
+}
+  
+  
